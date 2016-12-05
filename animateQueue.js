e2b5fd5dc8ff8349121e525f8e1f0b7d587e956f
韩文程 <hanwencheng@uzoo.cn>
2016-12-05 17:25:41 +0800

    init

---
+++ animateQueue.js
@@ -0,0 +1,67 @@
+/**
+ * Created by mario on 2016/12/5.
+ * 动作队列，可以连续的执行一连串动作，可以用来制作动画
+ */
+
+function EventHolder(){
+    this.listeners = {};
+}
+
+
+EventHolder.prototype = Object.prototype;
+
+EventHolder.prototype.bind = function(event, callback){
+    this.listeners[event] = callback;
+};
+
+EventHolder.prototype.publish = function(event, attrs){
+    if(this.listeners[event]){
+        this.listeners[event](this, event, attrs);
+    }
+};
+
+/**
+ * 动作队列
+ * @constructor
+ */
+function AnimateQueue(){
+    EventHolder.apply(this, arguments);
+    this.queue = [];
+    this.point = 0;
+    this.isStart = false;
+
+    this.bind("next", function(obj){
+        if(!obj.isStart){
+            return;
+        }
+        if(obj.point < obj.queue.length - 1){
+            obj.queue[obj.point]();
+        }else{
+            obj.isStart = false;
+            obj.publish("end");
+        }
+    });
+
+    this.bind("start", function(obj){
+        obj.isStart = true;
+        obj.publish("next");
+    });
+}
+
+AnimateQueue.prototype = EventHolder.prototype;
+
+AnimateQueue.prototype.add = function(callback, delay){
+    animateQueue = this;
+    this.queue.push(function(){
+        setTimeout(function(){
+            callback();
+            animateQueue.point++;
+            animateQueue.publish("next");
+        }, delay);
+    });
+
+};
+
+AnimateQueue.prototype.start = function(){
+    this.publish("start");
+};
